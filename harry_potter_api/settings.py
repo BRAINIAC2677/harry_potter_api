f08fa96a0d706d36b4d46fd7c45a8f73a274e967
BRAINIAC2677 <brainiac2677@gmail.com>
2022-09-03 23:37:16 +0600

    swagger and redoc added

--- harry_potter_api/settings.py
+++ harry_potter_api/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     # Third party apps
     'rest_framework',
+    'drf_yasg',
 
     # Local apps
     'characters',
--- harry_potter_api/urls.py
+++ harry_potter_api/urls.py
@@ -16,7 +16,28 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework import permissions
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_views = get_schema_view(
+    openapi.Info(
+        title="Harry Potter API",
+        default_version="v1",
+        description="A simple API for Harry Potter characters",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="asifazad0178@gmail.com"),   
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/characters/', include('characters.urls')),
+
+    path('swagger/', schema_views.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_views.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
